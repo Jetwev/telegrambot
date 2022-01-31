1470e9fc070b8528f6fb5b7c47e70b1d9ac99c7f
Artem <hi@mail.ru>
2022-01-31 16:36:24 +0300

    2.0

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.types import InlineKeyboardButton
-from aiogram.types import reply_keyboard
 
-from copy import deepcopy
 import os
-from urllib.parse import urljoin
+import numpy as np
+from io import BytesIO
 
 from config import *
+from transfer_style_class import *
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -21,27 +21,50 @@ class User_INFO:
     def __init__(self):
         self.photos = []
         self.wait_photos = None
+        self.imsize = 256
+        self.num_steps = 500
+        self.style_weight = 100000
+        self.content_weight = 0.1
+        self.params = None
+        self.default = 0
+    def change_imsize(self, i):
+        self.imsize = i
+    def change_num_steps(self, n):
+        self.num_steps = n
+    def change_style_weight(self, w):
+        self.style_weight = w
+    def change_content_weight(self, c):
+        self.content_weight = c
 
-buffer = {}
+users = {}
 
 start_kb = InlineKeyboardMarkup()
-start_kb.add( InlineKeyboardButton('Transfer style from one picture to another', callback_data='style') )
+start_kb.add(InlineKeyboardButton('Transfer style from one picture to another', callback_data='style'))
 
 settings_kb = InlineKeyboardMarkup()
-settings_kb.add( InlineKeyboardButton('Default', callback_data='default') )
-settings_kb.add( InlineKeyboardButton('Back', callback_data='menu') )
+settings_kb.add(InlineKeyboardButton('Default', callback_data='default'))
+settings_kb.add(InlineKeyboardButton('Personal', callback_data='personal'))
+settings_kb.add(InlineKeyboardButton('Reset settings', callback_data='reset'))
+settings_kb.add(InlineKeyboardButton('Back', callback_data='menu'))
 
 back_kb = InlineKeyboardMarkup()
-back_kb.add( InlineKeyboardButton('Back', callback_data='menu'))
+back_kb.add(InlineKeyboardButton('Back', callback_data='menu'))
+
+settings_personal_kb = InlineKeyboardMarkup()
+settings_personal_kb.add(InlineKeyboardButton('Image size', callback_data='imsize'))
+settings_personal_kb.add(InlineKeyboardButton('Number of steps', callback_data='num_steps'))
+settings_personal_kb.add(InlineKeyboardButton('Style weight', callback_data='style_weight'))
+settings_personal_kb.add(InlineKeyboardButton('Content weight', callback_data='content_weight'))
+settings_personal_kb.add(InlineKeyboardButton('Info', callback_data='info'))
+settings_personal_kb.add(InlineKeyboardButton('Next', callback_data='next'))
+settings_personal_kb.add(InlineKeyboardButton('Back', callback_data='menu'))
 
 #/start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, f"Hi {message.from_user.first_name}! I'm Style transfer bot.\n"
+    await bot.send_message(message.chat.id, f"Hi {message.from_user.first_name}! I'm Style-Transfer bot.\n"
     + "I can transfer style from one picture to another.\n" + "What I can:", reply_markup=start_kb)
-
-    buffer[message.chat.id] = User_INFO()
-
+    users[message.chat.id] = User_INFO()
 
 #/help
 @dp.message_handler(commands=['help'])
@@ -59,68 +82,160 @@ async def menu(callback_query):
 @dp.callback_query_handler(lambda c: c.data == 'style')
 async def style(callback_query):
     await bot.answer_callback_query(callback_query.id)
-    await callback_query.message.edit_text("Choose settings for transfer style:")
+    await callback_query.message.edit_text("Choose settings for transfer style:\n")
     await callback_query.message.edit_reply_markup(reply_markup = settings_kb)
-    if callback_query.from_user.id not in buffer:
-        buffer[callback_query.from_user.id] = User_INFO()
+    if callback_query.from_user.id not in users:
+        users[callback_query.from_user.id] = User_INFO()
 
 #default settings
 @dp.callback_query_handler(lambda c: c.data == 'default')
 async def default_set(callback_query):
     await bot.answer_callback_query(callback_query.id)
     await callback_query.message.edit_text("1-st picture -- STYLE\n" 
-    + "Send me a picture, the style from which you need to transfer.\n")
+    + "Send me a picture or document with a picture, the style from which you need to transfer.\n"
+    + "IMPORTANT: Use the same image file formats for both pictures.\n")
     await callback_query.message.edit_reply_markup(reply_markup=back_kb)
-    buffer[callback_query.from_user.id].wait_photos = 2
-    
+    users[callback_query.from_user.id].wait_photos = 2
+    users[callback_query.from_user.id].default = 0
+
+@dp.callback_query_handler(lambda c: c.data == 'next')
+async def personal_set(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text("1-st picture -- STYLE\n" 
+    + "Send me a picture or document with a picture, the style from which you need to transfer.\n"
+    + "IMPORTANT: Use the same image file formats for both pictures.\n")
+    await callback_query.message.edit_reply_markup(reply_markup=back_kb)
+    users[callback_query.from_user.id].wait_photos = 2
+    users[callback_query.from_user.id].default = 1
+
+@dp.callback_query_handler(lambda c: c.data == 'personal')
+async def personal_settings(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text("Choose the settings that you want to change.\n"
+    + "If you don't want to change then click Next to continue.\n")
+    await callback_query.message.edit_reply_markup(reply_markup=settings_personal_kb)
+
+@dp.callback_query_handler(lambda c: c.data == 'imsize')
+async def image_set(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text("Write the integer size of the image you would like to receive\n"
+    + "For example: 128, 256, 512, 1024, ...", reply_markup=back_kb)
+    users[callback_query.from_user.id].params = 1
+
+@dp.callback_query_handler(lambda c: c.data == 'num_steps')
+async def image_set(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text("Write the integer number of steps of the image you would like to receive\n"
+    + "For example: 250, 300, 500, ...", reply_markup=back_kb)
+    users[callback_query.from_user.id].params = 2
+
+@dp.callback_query_handler(lambda c: c.data == 'style_weight')
+async def image_set(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text("Write the style weight of the image you would like to receive\n"
+    + "Recommend to use big positive numbers. (100000 >= ...)\n", reply_markup=back_kb)
+    users[callback_query.from_user.id].params = 3
+
+@dp.callback_query_handler(lambda c: c.data == 'content_weight')
+async def image_set(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text("Write the content weight of the image you would like to receive\n"
+    +"Recommend to use small positive numbers. (... <= 1)", reply_markup=back_kb)
+    users[callback_query.from_user.id].params = 4
+
+@dp.message_handler(lambda message: message.text.isdigit())
+async def params(message: types.Message):
+    if users[message.chat.id].params == 1 and int(message.text) > 0:
+        users[message.chat.id].change_imsize(int(message.text))
+    elif users[message.chat.id].params == 2 and int(message.text) > 0:
+        users[message.chat.id].change_num_steps(int(message.text))
+    elif users[message.chat.id].params == 3 and float(message.text) > 0:
+        users[message.chat.id].change_style_weight(float(message.text))
+    elif users[message.chat.id].params == 4 and float(message.text) > 0:
+        users[message.chat.id].change_content_weight(float(message.text))
+    await bot.send_message(message.chat.id, "Would you like to change something else?\n"
+    + "Otherwise, click Next.\n", reply_markup=settings_personal_kb)
+
+@dp.message_handler(lambda message: not message.text.isdigit())
+async def is_valid(message: types.Message):
+    return await message.reply("Gotta be a number!\n (digits only)")
+
+@dp.callback_query_handler(lambda c: c.data == 'reset')
+async def menu(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    users[callback_query.from_user.id].imsize = 256
+    users[callback_query.from_user.id].num_steps = 500
+    users[callback_query.from_user.id].style_weight = 100000
+    users[callback_query.from_user.id].content_weight = 0.1
+    await callback_query.message.edit_text("Your personal settings have been reset.\n"
+    + "INFO: Image size = {}, Number of steps = {}, Style weight = {}, Content weight = {}".format(users[callback_query.from_user.id].imsize,
+    users[callback_query.from_user.id].num_steps, users[callback_query.from_user.id].style_weight, users[callback_query.from_user.id].content_weight), reply_markup=settings_kb)
+
+@dp.callback_query_handler(lambda c: c.data == 'info')
+async def menu(callback_query):
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text("INFO: Image size = {}, Number of steps = {}, Style weight = {}, Content weight = {}".format(users[callback_query.from_user.id].imsize,
+    users[callback_query.from_user.id].num_steps, users[callback_query.from_user.id].style_weight, users[callback_query.from_user.id].content_weight), reply_markup=settings_personal_kb)
+
 # getting image
 @dp.message_handler(content_types=['photo', 'document'])
 async def get_image(message):
     if message.content_type == 'photo':
         img = message.photo[-1]
-
     else:
         img = message.document
-        if img.mime_type[:5] != 'image':
-            await bot.send_message(message.chat.id,
-                "Upload a file in image format.",
-                reply_markup=start_kb)
+        if not set(":;!_*-+()#%&").isdisjoint(img.mime_type) or img.mime_type[:6] != 'image/':
+            await bot.send_message(message.chat.id, "Upload a file in image format.", reply_markup=start_kb)
             return
+    
+    file = await bot.get_file(img.file_id)
+    file_path = file.file_path
+    photo = await bot.download_file(file_path)
 
-    file_info = await bot.get_file(img.file_id)
-    photo = await bot.download_file(file_info.file_path)
+    users[message.chat.id].photos.append(photo)
 
-    buffer[message.chat.id].photos.append(photo)
+    if users[message.chat.id].wait_photos == 2:
+        users[message.chat.id].wait_photos = 1
+        await bot.send_message(message.chat.id,"2-d picture -- CONTENT\n" 
+        + "Send me a picture or document with picture to which you need to transfer this style.\n"
+        + "IMPORTANT: Use the same image file formats for both pictures.\n", reply_markup=back_kb)
 
-    if buffer[message.chat.id].wait_photos == 2:
-        buffer[message.chat.id].wait_photos = 1
-        await bot.send_message(message.chat.id,"2-d picture -- PICTURE\n" 
-        + "Send me a picture to which you need to transfer this style.",reply_markup=back_kb)
-
-    elif buffer[message.chat.id].wait_photos == 1:
+    elif users[message.chat.id].wait_photos == 1:
         await bot.send_message(message.chat.id, "In process")
-
-        # for debug
         try:
-            #output = await style_transfer(Style_Transfer_CLASS, buffer[message.chat.id], *buffer[message.chat.id].photos)
-            #await bot.send_document(message.chat.id, deepcopy(output))
-            await bot.send_photo(message.chat.id, buffer[message.chat.id].photos[1])
-
-        except RuntimeError as err:
-            if str(err)[:19] == 'CUDA out of memory.':
-                await bot.send_message(message.chat.id, "An error has occurred. I don't have enough memory to perform this action.")
-            else:
-                await bot.send_message(message.chat.id, "An error has occurred.")
+            input_img = await style_transfer(Style_Transfer, users[message.chat.id], users[message.chat.id].photos[0], users[message.chat.id].photos[1])
+            await bot.send_document(message.chat.id, ('image', deepcopy(input_img)))
+            await bot.send_photo(message.chat.id, input_img)
         except Exception as err:
-            await bot.send_message(message.chat.id, "An error has occurred.")
+            await bot.send_message(message.chat.id, "!! An error has occurred !!\nSome errors:\n"
+            + "1. Check that images have the same format.\n"
+            + "2. There may not be enough resources to process the image with your personal settings.\n")
 
         await bot.send_message(message.chat.id, "So what do we do next?", reply_markup=start_kb)
-    
-        del buffer[message.chat.id]
+        del users[message.chat.id]
 
+
+async def style_transfer(ST_Class, user, style_img, content_img):
+    if user.default == 0: 
+        st_class = ST_Class(style_img, content_img)
+    elif user.default == 1:
+        st_class = ST_Class(style_img, content_img, user.imsize, user.num_steps, user.style_weight, user.content_weight)
+    input_img = await st_class.run_style_transfer()
+    input_img = np.rollaxis(input_img.cpu().detach().numpy()[0], 0, 3)
+    input_img = Image.fromarray((input_img * 255).astype('uint8'))
+    input_img = img_to_media_obj(input_img)
+    return input_img  
+
+
+def img_to_media_obj(img):
+    img_byte_arr = BytesIO()
+    img.save(img_byte_arr, format='JPEG')
+    img_byte_arr.seek(0)
+    return img_byte_arr
+  
 
 async def on_startup(dp):
-    await bot.set_webhook(webhook_url)
+    await bot.set_webhook(WEBHOOK_URL)
     # insert code here to run it after start
 
 async def on_shutdown(dp):
@@ -135,18 +250,17 @@ if __name__ == '__main__':
         executor.start_polling(dp, skip_updates=True)
 
     elif CONNECTION_TYPE == 'WEBHOOK':
-        webhook_path = f'/webhook/{API_TOKEN}'
-        webhook_url  = f'{WEBHOOK_HOST}{webhook_path}'
-
-        webapp_host = '0.0.0.0'
-        webapp_port = int(os.environ.get('PORT', WEBAPP_PORT))
+        WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
+        WEBHOOK_URL  = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+        WEBAPP_HOST = '0.0.0.0'
+        WEBAPP_PORT = os.environ.get('PORT', 3001)
 
         executor.start_webhook(
             dispatcher=dp,
-            webhook_path=webhook_path,
+            webhook_path=WEBHOOK_PATH,
             on_startup=on_startup,
             on_shutdown=on_shutdown,
             skip_updates=True,
-            host=webapp_host,
-            port=webapp_port,
+            host=WEBAPP_HOST,
+            port=WEBAPP_PORT,
         )
---
+++ transfer_style_class.py
@@ -0,0 +1,173 @@
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as transforms
+
+from copy import deepcopy
+
+#===========================================================================
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cpu")
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+content_layers_default = ['conv_4']
+style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+model_vgg19 = torch.load('model/vgg19.pth',).to(device).eval()
+#===========================================================================
+
+class ContentLoss(nn.Module):
+    def __init__(self, target,):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+        self.loss = F.mse_loss(self.target, self.target )
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = self.gram_matrix(target_feature).detach()
+        self.loss = F.mse_loss(self.target, self.target)
+    
+    def gram_matrix(self, input):
+        batch_size , h, w, f_map_num = input.size()
+        features = input.view(batch_size * h, w * f_map_num)
+        G = torch.mm(features, features.t())
+        return G.div(batch_size * h * w * f_map_num)
+
+    def forward(self, input):
+        G = self.gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+
+class Normalization(nn.Module):
+        def __init__(self, mean, std):
+            super(Normalization, self).__init__()
+            self.mean = mean.detach().clone().view(-1, 1, 1)
+            self.std = std.detach().clone().view(-1, 1, 1)
+
+        def forward(self, img):
+            return (img - self.mean) / self.std
+
+
+class Style_Transfer:
+    def __init__(self, style_img, content_img, imsize = 256, num_steps=500,
+                    style_weight=100000, content_weight=0.1):
+
+        self.imsize = imsize
+        self.style_img = self.image_loader(style_img)
+        self.content_img = self.image_loader(content_img)
+        self.input_img = self.content_img.clone()
+        self.num_steps = num_steps
+        self.style_weight = style_weight
+        self.content_weight = content_weight
+
+
+    def image_loader(self, image_name):
+        loader = transforms.Compose([
+        transforms.Resize(self.imsize),
+        transforms.CenterCrop(self.imsize),
+        transforms.ToTensor()])
+        image = Image.open(image_name)
+        image = loader(image).unsqueeze(0)
+        return image.to(device, torch.float)
+
+
+    def get_style_model_and_losses(self, normalization_mean, normalization_std, content_layers=content_layers_default, style_layers=style_layers_default):
+        cnn = deepcopy(model_vgg19)
+        normalization = Normalization(normalization_mean, normalization_std).to(device)
+                                   
+        content_losses = []
+        style_losses = []
+
+        model = nn.Sequential(normalization)
+
+        i = 0
+        for layer in cnn.children():
+            if isinstance(layer, nn.Conv2d):
+                i += 1
+                name = 'conv_{}'.format(i)
+            elif isinstance(layer, nn.ReLU):
+                name = 'relu_{}'.format(i)
+                layer = nn.ReLU(inplace=False)
+            elif isinstance(layer, nn.MaxPool2d):
+                name = 'pool_{}'.format(i)
+            elif isinstance(layer, nn.BatchNorm2d):
+                name = 'bn_{}'.format(i)
+            else:
+                raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+            model.add_module(name, layer)
+
+            if name in content_layers:
+                target = model(self.content_img).detach()
+                content_loss = ContentLoss(target)
+                model.add_module("content_loss_{}".format(i), content_loss)
+                content_losses.append(content_loss)
+
+            if name in style_layers:
+                target_feature = model(self.style_img).detach()
+                style_loss = StyleLoss(target_feature)
+                model.add_module("style_loss_{}".format(i), style_loss)
+                style_losses.append(style_loss)
+
+        for i in range(len(model) - 1, -1, -1):
+            if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+                break
+
+        model = model[:(i + 1)]
+
+        return model, style_losses, content_losses
+
+
+    def get_input_optimizer(self):
+        optimizer = optim.LBFGS([self.input_img.requires_grad_()]) 
+        return optimizer
+
+    async def run_style_transfer(self):
+        model, style_losses, content_losses = self.get_style_model_and_losses(cnn_normalization_mean, cnn_normalization_std)
+        optimizer = self.get_input_optimizer()
+        run = [0]
+        while run[0] <= self.num_steps:
+            def closure():
+                self.input_img.data.clamp_(0, 1)
+
+                optimizer.zero_grad()
+
+                model(self.input_img)
+
+                style_score = 0
+                content_score = 0
+
+                for sl in style_losses:
+                    style_score += sl.loss
+                for cl in content_losses:
+                    content_score += cl.loss
+                
+                style_score *= self.style_weight
+                content_score *= self.content_weight
+
+                loss = style_score + content_score
+                loss.backward()
+
+                run[0] += 1
+                if run[0] % 50 == 0:
+                    print("run {}\n".format(run))
+
+                return style_score + content_score
+
+            optimizer.step(closure)
+
+
+        self.input_img.data.clamp_(0, 1)
+
+        return self.input_img
